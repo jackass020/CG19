0f03825dfa355c8d2d6d4ead61cb34b487aebac3
Nelson Gonçalves <nelson@pc.lan>
2019-03-03 22:26:22 +0000

    trabalho

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,15 +1,107 @@
+#include <stdlib.h>
+
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
 
-#define _USE_MATH_DEFINES
-#include <math.h>
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include <set>
+#include "tinyxml2.h"
+
+using namespace std;
+
+typedef std::string Path;
+
+typedef struct coordinate {
+	float x;
+	float y;
+	float z;
+}Vertex;
+
+typedef std::vector<Vertex> Triangle;
+
+typedef std::vector<Triangle> Model;
+
+typedef std::set<Model> Models;
 
 float alpha;
 float beta;
 float radius = 5;
+
+string paths [15];
+int paths_size = 0;
+
+void readXML(){
+    XMLDocument doc;
+    int i = 0;
+    doc.LoadFile("./Config.xml");
+    XMLElement* root = doc.FirstChildElement();
+    const char* type;
+    for(XMLElement* elem = root->FirstChildElement();elem != NULL; elem = elem->NextSiblingElement()) {
+        string nome_elem = elem->Value();
+        if(nome_elem == "model") {
+            type = elem->Attribute("file");
+            if(type != NULL) {
+				paths[i] = type;
+                i++;
+            }
+        }
+    }
+    paths_size = i;
+}
+
+
+void loadXML() {
+	float x, y, z;
+	int i = 0,j=0;
+	string c1, c2, c3;
+	Model model;
+	for (i=0;i<paths_size;i++) {
+        Path p = paths[i];
+		ifstream file(p);
+		if (file.is_open()) {
+			Triangle t;
+			while (getline(file, c1) && getline(file, c2) && getline(file, c3)) {
+				x = strtof(c1.c_str(),0);
+				y = strtof(c2.c_str(),0);
+				z = strtof(c3.c_str(),0);
+				Vertex v = {x,y,z};
+				t.push_back(v);
+				j++;
+				if (j % 3 == 0) {
+					model.push_back(t);
+					t.clear();
+				}
+			}
+			file.close();
+		}
+		models.insert(model);
+		model.clear();
+	}
+}
+
+void drawTheFiles(){
+    int i=0;
+	for (Model m : models) {
+		glBegin(GL_TRIANGLES);
+		for (Triangle t : m) {
+            if(i%2 == 0)
+                glColor3f(0,1,1);
+            else glColor3f(1,0,0);
+			glVertex3f(t.at(0).x, t.at(0).y, t.at(0).z);
+			glVertex3f(t.at(1).x, t.at(1).y, t.at(1).z);
+			glVertex3f(t.at(2).x, t.at(2).y, t.at(2).z);
+            i++;
+		}
+		glEnd();
+	}
+}
 
 void changeSize(int w, int h) {
 
@@ -17,14 +109,14 @@ void changeSize(int w, int h) {
 	// (you cant make a window with zero width).
 	if(h == 0) h = 1;
 
-	// compute window's aspect ratio 
+	// compute window's aspect ratio
 	float ratio = w * 1.0 / h;
 
 	// Set the projection matrix as current
 	glMatrixMode(GL_PROJECTION);
 	// Load Identity Matrix
 	glLoadIdentity();
-	
+
 	// Set the viewport to be the entire window
     glViewport(0, 0, w, h);
 
@@ -50,12 +142,13 @@ void renderScene(void) {
 
 	// set the camera
 	glLoadIdentity();
-	gluLookAt(xaxis,yaxis,zaxis, 
+	gluLookAt(xaxis,yaxis,zaxis,
 		      0.0,0.0,-1.0,
 			  0.0f,1.0f,0.0f);
 
 // put drawing instructions here
-	glutWireTeapot(0.5f);
+	drawTheFiles();
+
 
 	// End of frame
 	glutSwapBuffers();
@@ -90,6 +183,10 @@ void process_keys(unsigned char key, int x, int y) {
 
 int main(int argc, char **argv) {
 
+
+  readXML();
+  loadXML();
+
 	alpha = 0;
 	beta = 3;
 
@@ -108,14 +205,13 @@ int main(int argc, char **argv) {
 	glutKeyboardFunc(process_keys);
 
 
-// OpenGL settings 
+// OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
-	
+
 
 // enter GLUT's main loop
 	glutMainLoop();
-	
+
 	return 1;
 }
-
